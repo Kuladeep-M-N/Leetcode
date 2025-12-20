8390f9e5865a57a5691d14f239ce8879d0a7c4b1
Kuladeep M N <kuladeepmn13@gmail.com>
2025-12-20 20:53:45 +0530

    Time: 61 ms (63.39%) | Memory: 17.5 MB (99.96%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,21 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def is_pal(l, r):
+        def ispal(l,r):
             while l < r:
                 if s[l] != s[r]:
                     return False
-                l += 1
-                r -= 1
+                l+=1
+                r-=1
             return True
 
-        left, right = 0, len(s) - 1
-
+        left = 0
+        right = len(s) - 1
         while left < right:
             if s[left] == s[right]:
                 left += 1
                 right -= 1
             else:
-                # Try deleting either left or right ONCE
-                return is_pal(left + 1, right) or is_pal(left, right - 1)
-
+                return ispal(left+1,right) or ispal(left,right-1)
         return True
