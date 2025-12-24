a7b09c438a383a46941dabef25cffd52ebd57e78
Kuladeep M N <kuladeepmn13@gmail.com>
2025-12-24 22:42:46 +0530

    Time: 55 ms (6.20%) | Memory: 17.7 MB (98.23%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -3,21 +3,18 @@ class Solution:
         st = ""
         te = ""
         left=-1
-        right=0
         for i in range(len(s)):
-            right+=1
             if s[i] == " ":
                 left += 1
-                for j in range(left,right-1):
+                for j in range(left,i):
                     st = s[j] + st 
                     left += 1 
                 st += " "
                 te += st
-                st = ""
-                
-            elif right == len(s):
+                st = "" 
+            elif i == len(s)-1:
                 left += 1
-                for k in range(left,right):
+                for k in range(left,i+1):
                     st = s[k] + st
                 te += st
         return te
